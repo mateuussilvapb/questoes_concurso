3e5f7db41e8c02a641cbc024ab028e6c89b35567
Mateus Dias <mateuussilvapb@gmail.com>
2026-06-24 18:20:43 -0300

    FEAT: - Configurações iniciais - Configurações do primeng - Configurações do primeflex - Configurações do primeicons - Configurações gerais de layout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,30 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { primeNgTranslation } from './i18n/primeng-pt';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+      translation: primeNgTranslation,
+      zIndex: {
+        modal: 1100, // dialog, sidebarc
+        overlay: 900, // dropdown, overlaypanel
+        menu: 1000, // overlay menus
+        tooltip: 1100, // tooltip
+      },
+    }),
+  ],
 };
