ffb247599ddda0e0b1b144480ce7758405b3b6c3
Mateus Dias <mateuussilvapb@gmail.com>
2026-06-26 20:50:47 -0300

    FEAT: - Base MateriasFormPage

---
+++ src/app/pages/materias/components/materias-form-page/materias-form-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-materias-form-page',
+  imports: [],
+  templateUrl: './materias-form-page.html',
+})
+export class MateriasFormPage {}
