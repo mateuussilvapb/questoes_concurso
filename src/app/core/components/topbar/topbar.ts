7b77b38b95956c4315f482e74c2ae36d48b45a28
Mateus Dias <mateuussilvapb@gmail.com>
2026-06-26 19:41:31 -0300

    FEAT: - Logo aplicação - Ajustes angular.json para indexação do assets

--- src/app/core/components/topbar/topbar.ts
+++ src/app/core/components/topbar/topbar.ts
@@ -1,13 +1,14 @@
 //Angular
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 //Externos
 import { ButtonModule } from 'primeng/button';
 
 //Aplicação
 import { LayoutService } from '../../services/layout.service';
-import { TopbarMenuOptions } from './topbar-menu-options/topbar-menu-options';
 import { SidebarMobile } from './sidebar-mobile/sidebar-mobile';
+import { TopbarMenuOptions } from './topbar-menu-options/topbar-menu-options';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-topbar',
@@ -23,4 +24,9 @@ import { SidebarMobile } from './sidebar-mobile/sidebar-mobile';
 })
 export class Topbar {
   protected readonly layoutService = inject(LayoutService);
+  protected readonly themeService = inject(ThemeService);
+
+  logoByTheme = computed<string>(() => {
+    return this.themeService.isDarkMode() ? 'assets/imgs/logo_dark.png' : 'assets/imgs/logo.png';
+  });
 }
