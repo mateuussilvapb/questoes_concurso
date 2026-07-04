4639ccc61c0028263893e8fb1f8e0017d2b2636d
Mateus Dias <mateuussilvapb@gmail.com>
2026-07-03 21:03:27 -0300

    FEAT: - Ajustes componente de alternativa - Ajustes coloração background

--- src/app/pages/questoes/components/alternativa/alternativa.ts
+++ src/app/pages/questoes/components/alternativa/alternativa.ts
@@ -1,10 +1,15 @@
-// alternativa.ts
+//Angular
 import { CommonModule } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ButtonModule } from 'primeng/button';
+import { Component, computed, effect, inject, input, output } from '@angular/core';
+
+//Aplicação
+import { ThemeService } from '../../../../core/services/theme.service';
 
 // Externo
+import { ButtonModule } from 'primeng/button';
+import { of, startWith, switchMap } from 'rxjs';
 import { InputTextModule } from 'primeng/inputtext';
 import { RadioButtonClickEvent, RadioButtonModule } from 'primeng/radiobutton';
 
@@ -24,6 +29,8 @@ import { RadioButtonClickEvent, RadioButtonModule } from 'primeng/radiobutton';
   templateUrl: './alternativa.html',
 })
 export class Alternativa {
+  private readonly themeService = inject(ThemeService);
+
   index = input.required<number>();
   formGroup = input.required<FormGroup>();
   qtdAlternativas = input.required<number>();
@@ -31,6 +38,21 @@ export class Alternativa {
 
   removeAlternativa = output<number>();
   selectAlternativa = output<number>();
+
+  constructor() {
+    effect(() => {
+      const isMultiplaEscolha = this.tipoQuestaoMultiplaEscolha();
+      this.handleChangeTipoQuestao(isMultiplaEscolha);
+    });
+  }
+
+  handleChangeTipoQuestao(isMultiplaEscolha: boolean) {
+    if (isMultiplaEscolha) {
+      this.formGroup().get('texto')?.enable();
+      return;
+    }
+    this.formGroup().get('texto')?.disable();
+  }
 
   mapIndexToLetter(index: number): string {
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -42,4 +64,27 @@ export class Alternativa {
     event.originalEvent.stopPropagation();
     this.selectAlternativa.emit(this.index());
   }
+
+  private readonly isCorretaSignal = toSignal(
+    toObservable(this.formGroup).pipe(
+      switchMap((form) => {
+        const control = form.get('correta');
+        if (!control) return of(false);
+
+        return control.valueChanges.pipe(startWith(control.value));
+      }),
+    ),
+    { initialValue: false },
+  );
+
+  backgroundColorAlternativaCorreta = computed(() => {
+    const isCorreta = this.isCorretaSignal();
+    if (!isCorreta) return '';
+
+    const isDarkMode = this.themeService.isDarkMode();
+    if (isDarkMode) {
+      return 'bg-green-700 text-0 font-bold';
+    }
+    return 'bg-green-50 font-bold';
+  });
 }
