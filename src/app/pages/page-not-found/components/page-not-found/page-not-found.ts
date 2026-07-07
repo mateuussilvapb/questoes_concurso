7ad05448609903bb44b692f55f7a00e920aa96c5
Mateus Dias <mateuussilvapb@gmail.com>
2026-07-07 10:37:27 -0300

    FEAT: - ajustes no componente de página não encontrada

--- src/app/pages/page-not-found/components/page-not-found/page-not-found.ts
+++ src/app/pages/page-not-found/components/page-not-found/page-not-found.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+//Angular
+import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [RouterLink],
+  imports: [
+    //Angular
+
+    //Externo
+    ButtonModule,
+  ],
   templateUrl: './page-not-found.html',
   styleUrl: './page-not-found.scss',
 })
-export class PageNotFound {}
+export class PageNotFound {
+  private readonly router = inject(Router);
+
+  telaInicial() {
+    this.router.navigate(['/']);
+  }
+}
